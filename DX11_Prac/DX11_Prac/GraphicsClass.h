a2cccf8f2f3be1c912e935c82ef2de9910b999ce
kencoor <kencoor@naver.com>
2021-07-17 21:05:14 +0900

    Direct3D 초기화

--- DX11_Prac/DX11_Prac/GraphicsClass.h
+++ DX11_Prac/DX11_Prac/GraphicsClass.h
@@ -8,6 +8,9 @@ const bool VSYNC_ENABLED = true;
 const float SCREEN_DEPTH = 1000.0f;
 const float SCREEN_NEAR = 0.1f;
 
+
+class D3DClass;
+
 class GraphicsClass
 {
 public:
@@ -21,4 +24,7 @@ public:
 
 private:
 	bool Render();
+
+private:
+	D3DClass* m_Direct3D = nullptr;
 };
